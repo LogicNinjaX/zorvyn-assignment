174996392c1319871639609bc28dfc2deffc54cd
Nitish <mail.logicninjax@gmail.com>
2026-04-03 13:56:45 +0530

    initial commit

---
+++ src/main/java/com/nitish/zorvyn_assignment/ZorvynAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.nitish.zorvyn_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZorvynAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZorvynAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nitish/zorvyn_assignment/ZorvynAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nitish.zorvyn_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZorvynAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
