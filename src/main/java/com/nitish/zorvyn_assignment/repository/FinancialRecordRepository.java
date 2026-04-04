fdf5a944e1ce3fc9b480d8d3dda272981398ae67
Nitish <mail.logicninjax@gmail.com>
2026-04-04 11:29:00 +0530

    add jpql query method

--- src/main/java/com/nitish/zorvyn_assignment/repository/FinancialRecordRepository.java
+++ src/main/java/com/nitish/zorvyn_assignment/repository/FinancialRecordRepository.java
@@ -1,9 +1,26 @@
 package com.nitish.zorvyn_assignment.repository;
 
 import com.nitish.zorvyn_assignment.entity.FinancialRecord;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface FinancialRecordRepository extends JpaRepository<FinancialRecord, UUID> {
+
+    @Query("""
+            SELECT fr FROM FinancialRecord fr
+            JOIN FETCH createdBy
+            WHERE fr.recordId = :recordId
+            """)
+    Optional<FinancialRecord> findRecordById(UUID recordId);
+
+    @Query("""
+            SELECT fr FROM FinancialRecord fr
+            JOIN FETCH createdBy
+            """)
+    Page<FinancialRecord> findAllRecord(Pageable pageable);
 }
