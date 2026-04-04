2895b0f7c6bfb0b820e01882fae941e4c40465fd
Nitish <mail.logicninjax@gmail.com>
2026-04-04 20:40:13 +0530

    add security config

---
+++ src/main/java/com/nitish/zorvyn_assignment/config/SecurityConfig.java
@@ -0,0 +1,79 @@
+package com.nitish.zorvyn_assignment.config;
+
+import com.nitish.zorvyn_assignment.security.JWTAuthenticationFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    private final AuthenticationEntryPoint authenticationEntryPoint;
+    private final JWTAuthenticationFilter jwtAuthenticationFilter;
+
+    public SecurityConfig(AuthenticationEntryPoint authenticationEntryPoint, JWTAuthenticationFilter jwtAuthenticationFilter) {
+        this.authenticationEntryPoint = authenticationEntryPoint;
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity security) throws Exception {
+        security.cors(AbstractHttpConfigurer::disable);
+
+        security.csrf(AbstractHttpConfigurer::disable);
+
+        security.authorizeHttpRequests(
+                requestMatcher ->
+                        requestMatcher.requestMatchers(
+                                        "/api/v1/auth/**"
+                                ).permitAll()
+                                .anyRequest().authenticated()
+        );
+
+        security.exceptionHandling(exceptionConfig ->
+                exceptionConfig.authenticationEntryPoint(authenticationEntryPoint)
+        );
+
+        security.sessionManagement(
+                sessionConfig ->
+                        sessionConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        );
+
+        security.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return security.build();
+    }
+
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(12);
+    }
+
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(UserDetailsService service, PasswordEncoder encoder) {
+        var authenticationProvider = new DaoAuthenticationProvider(service);
+        authenticationProvider.setPasswordEncoder(encoder);
+        return authenticationProvider;
+    }
+}
