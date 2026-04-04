a2c7a65030e9aa0b741d9d5e45ea2e598ca0da2d
Nitish <mail.logicninjax@gmail.com>
2026-04-04 20:42:04 +0530

    add jpql query methods

--- src/main/java/com/nitish/zorvyn_assignment/repository/FinancialRecordRepository.java
+++ src/main/java/com/nitish/zorvyn_assignment/repository/FinancialRecordRepository.java
@@ -1,11 +1,16 @@
 package com.nitish.zorvyn_assignment.repository;
 
+import com.nitish.zorvyn_assignment.dto.CategorySummaryDTO;
+import com.nitish.zorvyn_assignment.dto.MonthlySummaryDTO;
 import com.nitish.zorvyn_assignment.entity.FinancialRecord;
+import com.nitish.zorvyn_assignment.enums.RecordType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.math.BigDecimal;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -23,4 +28,27 @@ public interface FinancialRecordRepository extends JpaRepository<FinancialRecord
             JOIN FETCH createdBy
             """)
     Page<FinancialRecord> findAllRecord(Pageable pageable);
+
+    @Query("""
+                SELECT COALESCE(SUM(fr.amount), 0)
+                FROM FinancialRecord fr
+                WHERE fr.type = :type
+            """)
+    BigDecimal fetchTotalAmountByType(RecordType type);
+
+    @Query("""
+            SELECT fr.category AS category, COALESCE(SUM(fr.amount), 0) AS total
+            FROM FinancialRecord fr
+            GROUP BY fr.category
+            """)
+    List<CategorySummaryDTO> getCategorySummary();
+
+    @Query("""
+                SELECT FUNCTION('MONTH', fr.timeStamp) AS month, COALESCE(SUM(fr.amount), 0) AS total
+                FROM FinancialRecord fr
+                WHERE fr.type = :type
+                GROUP BY FUNCTION('MONTH', fr.timeStamp)
+                ORDER BY month
+            """)
+    List<MonthlySummaryDTO> getMonthlySummary(RecordType type);
 }
