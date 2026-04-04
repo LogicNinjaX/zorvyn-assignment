56212d7de505d3e67ddbfc15620513a6a1dc9744
Nitish <mail.logicninjax@gmail.com>
2026-04-04 20:44:13 +0530

    add mapper method

--- src/main/java/com/nitish/zorvyn_assignment/util/mapper/UserMapper.java
+++ src/main/java/com/nitish/zorvyn_assignment/util/mapper/UserMapper.java
@@ -1,9 +1,11 @@
 package com.nitish.zorvyn_assignment.util.mapper;
 
+import com.nitish.zorvyn_assignment.dto.request.UserRegisterRequest;
 import com.nitish.zorvyn_assignment.dto.response.UserDetailsResponse;
 import com.nitish.zorvyn_assignment.dto.response.UserUpdateResponse;
 import com.nitish.zorvyn_assignment.entity.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
@@ -11,4 +13,12 @@ public interface UserMapper {
     UserUpdateResponse toUpdateResponse(User user);
 
     UserDetailsResponse toDetailsResponse(User user);
+
+    @Mapping(target = "userId", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "status", ignore = true)
+    @Mapping(target = "financialRecords", ignore = true)
+    @Mapping(target = "deleted", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    User toUser(UserRegisterRequest request);
 }
