d1cb60b198ecf21e8c5b2975831c26f0dc427972
Nitish <mail.logicninjax@gmail.com>
2026-04-05 10:38:11 +0530

    add custom exception for invalid category

---
+++ src/main/java/com/nitish/zorvyn_assignment/exception/InvalidCategoryException.java
@@ -0,0 +1,7 @@
+package com.nitish.zorvyn_assignment.exception;
+
+public class InvalidCategoryException extends RuntimeException {
+    public InvalidCategoryException(String message) {
+        super(message);
+    }
+}
